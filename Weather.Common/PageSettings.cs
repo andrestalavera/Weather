4bd0405e6b932de031e8f0d23a302f0e83838fd6
Andres Talavera <andres.talavera@live.fr>
2021-03-24 11:05:59 +0100

    Add pagination project

---
+++ Weather.Common/PageSettings.cs
@@ -0,0 +1,8 @@
+namespace Weather.Common
+{
+    public static class PageSettings
+    {
+        public const int DEFAULT_PAGE_SIZE = 10;
+        public const int DEFAULT_PAGE_NUMBER = 1;
+    }
+}
